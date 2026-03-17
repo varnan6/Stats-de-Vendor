266e36f3a41fc8e920ea830bdf9bff818e0246ed
Varnan Rathod <varnanrathod6@gmail.com>
2026-03-17 18:32:33 +0530

    Add main script

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from app.agent.router import route_query
+
+def main():
+
+    print("=== Vendor Analytics Agent ===")
+
+    while True:
+
+        query = input("\n> ")
+
+        if query.lower() in ["exit", "quit"]:
+            break
+
+        result = route_query(query)
+
+        print("\nResult:\n", result)
+
+
+if __name__ == "__main__":
+    main()
